0bd94f919d2e0614beef1e66d9a3228a705a18e7
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2026-06-04 13:20:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
- const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+ // const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 
-    const buttonsContainer = document.getElementById("buttons");
+ //    const buttonsContainer = document.getElementById("buttons");
 
-    sounds.forEach((sound) => {
-      const button = document.createElement("button");
-      button.classList.add("btn");
-      button.innerText = sound;
+ //    sounds.forEach((sound) => {
+ //      const button = document.createElement("button");
+ //      button.classList.add("btn");
+ //      button.innerText = sound;
 
-      const audio = new Audio(`sounds/${sound}.mp3`);
+ //      const audio = new Audio(`sounds/${sound}.mp3`);
 
-      button.addEventListener("click", () => {
-        stopSongs();
-        audio.play();
-      });
+ //      button.addEventListener("click", () => {
+ //        stopSongs();
+ //        audio.play();
+ //      });
 
-      buttonsContainer.appendChild(button);
-    });
+ //      buttonsContainer.appendChild(button);
+ //    });
 
-    const stopButton = document.createElement("button");
-    stopButton.classList.add("stop");
-    stopButton.innerText = "stop";
+ //    const stopButton = document.createElement("button");
+ //    stopButton.classList.add("stop");
+ //    stopButton.innerText = "stop";
 
-    stopButton.addEventListener("click", stopSongs);
+ //    stopButton.addEventListener("click", stopSongs);
 
-    buttonsContainer.appendChild(stopButton);
+ //    buttonsContainer.appendChild(stopButton);
 
-    function stopSongs() {
-      sounds.forEach((sound) => {
-        const audio = new Audio(`sounds/${sound}.mp3`);
-        audio.pause();
-        audio.currentTime = 0;
-      });
-    }
+ //    function stopSongs() {
+ //      sounds.forEach((sound) => {
+ //        const audio = new Audio(`sounds/${sound}.mp3`);
+ //        audio.pause();
+ //        audio.currentTime = 0;
+ //      });
+ //    }
